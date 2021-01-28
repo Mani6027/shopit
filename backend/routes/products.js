ef9b8f685933974fc5425709d818fba511b72235
mani6027 <manisaraha@gmail.com>
2021-01-28 22:21:43 +0530

    Add - Get single product route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProducts, newProduct} = require('../controllers/productControllers');
+const {getProducts, newProduct, getSingleProduct} = require('../controllers/productControllers');
 
 router.route('/products').get(getProducts);
 
 router.route('/product/new').post(newProduct);
+
+router.route('/product/:id').get(getSingleProduct);
 
 module.exports = router;
