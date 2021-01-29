682e9a31775c123bd647ec39fb2e6cc719906caa
mani6027 <manisaraha@gmail.com>
2021-01-29 13:23:22 +0530

    Add error handler for prodcution and development

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,8 @@
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 
+const ErrorHandler = require('../utils/errorHandler');
+
 // Create new prodcut => /api/v1/product/new
 exports.newProduct = async(req, res, next) => {
     
@@ -33,10 +35,7 @@ exports.getSingleProduct = async (req, res, next) => {
         const product = await Product.findById(req.params.id);
         
         if(!product){
-            return res.status(404).json({
-                success: false,
-                message: 'Product not found'
-            })
+            return next(new ErrorHandler('Product not found',  404))
         }
 
         res.status(200).json({
@@ -63,13 +62,13 @@ exports.updateProdcut = async (req, res, next) => {
                 message: 'Product not found'
             })
         }
-        
+
         product = await Product.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true,
             useFindAndModify: false
         })
-        
+
         res.status(200).json({
             success: true,
             product
@@ -82,7 +81,6 @@ exports.updateProdcut = async (req, res, next) => {
         })
     }
 }
-
 
 // Deleted product  => /api/v1/admin/product/:id
 exports.deleteProduct = async (req, res, next) => {
