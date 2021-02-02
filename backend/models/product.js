3b261cefe82704c11b99a3dc5b446e11df792edf
mani6027 <manisaraha@gmail.com>
2021-02-02 10:49:52 +0530

    Add user relation to product

--- backend/models/product.js
+++ backend/models/product.js
@@ -80,6 +80,11 @@ const productSchema = new mongoose.Schema({
              required: true
          }
     }],
+    user:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true,
+    },
     createdAt:{
         type: Date,
         default: Date.now()
