907c7fb10f56aed47856a0b82cc8726d5687f0f2
mani6027 <manisaraha@gmail.com>
2021-02-02 11:04:02 +0530

    Generate reset token for user to reset password

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const JWT = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -61,6 +62,20 @@ userSchema.methods.jwtToken = function() {
     return JWT.sign({id: this.id}, process.env.JWT_PRIVATE_KEY, {
         expiresIn: process.env.JWT_EXPIRATION_TIME
     });
+}
+
+//Generate password reset token
+userSchema.methods.getResetPasswordToken = function(){
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // Set expire token
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
 }
 
 module.exports = mongoose.model('User', userSchema);
