bc5c63e7595857cb6c5a7837b3bf30c5bed781ab
mani6027 <manisaraha@gmail.com>
2021-02-02 14:31:39 +0530

    Add user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,19 +6,24 @@ const {
     loginUser, 
     logout, 
     forgotPassword,
-    resetPassword} = require('../controllers/usersController');
+    resetPassword,
+    getUserProfile} = require('../controllers/usersController');
+
+const {isAuthentcatedUser} = require('../middlewares/auth');
 
 router.route('/health').get((req, res, next) => {
     res.status(200).json({
         success: true,
         message: 'Auth route is healthy'})})
 
-router.route('/logout').get(logout);
-
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/logout').get(logout);
+
+router.route('/me').get(isAuthentcatedUser, getUserProfile);
 
 
 module.exports = router;
